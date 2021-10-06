63aac3f39907c946459907fbd01b83d75b1b448c
Calenzo <carlos.calenzo@stormgroup.com.br>
2021-10-06 14:56:19 -0300

    first commit

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import { Home } from "./pages"
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home}/>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export { Routes };
