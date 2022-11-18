829204c2b924cc30176fa9d02173583f64bbaceb
Calenzo <calenzodevs@gmail.com>
2022-11-18 15:06:37 -0300

    feat: Removed the notification component, because be use the lib of notification

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
 import { Button } from "./Button";
 import { Input } from "./Input";
-import { Notification } from "./Notification";
 import { Main } from "./Main";
 import { ModalConectBank } from "./ModalConectBank";
 import { Select } from "./Select";
 
-export { Button, Input, Notification, Main, ModalConectBank, Select };
+export { Button, Input, Main, ModalConectBank, Select };
