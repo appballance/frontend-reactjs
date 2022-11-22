5f28e7868e3abc904124e66889deeb3f2843fef4
Calenzo <calenzodevs@gmail.com>
2022-11-22 20:07:57 -0300

    refactor: structure of form of connect with new bank

--- src/views/pages/HomeAuthenticated/FormModalConnectBank/FormBanks/index.js
+++ src/views/pages/HomeAuthenticated/FormModalConnectBank/FormBanks/index.js
@@ -1,17 +1,31 @@
-import React, { useCallback } from "react";
+import React from "react";
+import { useFormikContext } from "formik";
 
-import { CODE_BANK } from "application/mocks";
+import { Input } from "views/components";
 
-import { Nubank } from "./Nubank";
+import { useFormBanks } from "./useFormBanks";
 
 export const FormBanks = ({ codeBank = null, stateCode }) => {
-  const formBanks = {
-    [CODE_BANK.NUBANK]: useCallback(() => <Nubank stateCode={stateCode} />, []),
-    [CODE_BANK.BRAZIL_BANK]: useCallback(() => <></>, []),
-    [CODE_BANK.PICPAY]: useCallback(() => <></>, []),
-    [null]: useCallback(() => <></>, []),
-  };
-  const FormBankComponent = formBanks[codeBank];
+  const { setFieldValue, values } = useFormikContext();
+  const { getCurrentBankStyle } = useFormBanks();
 
-  return <FormBankComponent />;
+  return (
+    <>
+      <Input
+        hide={getCurrentBankStyle(codeBank)?.hide || stateCode}
+        name="cpf"
+        label="CPF"
+        value={values["cpf"]}
+        onChange={(e) => setFieldValue("cpf", e.target.value)}
+      />
+      <Input
+        hide={getCurrentBankStyle(codeBank)?.hide || stateCode}
+        name="password"
+        label="Senha"
+        type="password"
+        value={values["password"]}
+        onChange={(e) => setFieldValue("password", e.target.value)}
+      />
+    </>
+  );
 };
---
+++ src/views/pages/HomeAuthenticated/FormModalConnectBank/FormBanks/useFormBanks.js
@@ -0,0 +1,24 @@
+import { CODE_BANK } from "application/mocks";
+
+export const useFormBanks = () => {
+  const stylesBanks = {
+    [CODE_BANK.NUBANK]: {
+      hide: false,
+    },
+    [CODE_BANK.BRAZIL_BANK]: {
+      hide: true,
+    },
+    [CODE_BANK.PICPAY]: {
+      hide: true,
+    },
+    [null]: {
+      hide: true,
+    },
+  };
+
+  const getCurrentBankStyle = (currentBankCode = null) => stylesBanks[currentBankCode];
+
+  return {
+    getCurrentBankStyle,
+  };
+};
